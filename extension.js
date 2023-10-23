aec5f903e47a3c5ba2b5721d20b8fb5e36840bcb
John Ward <John.Ward@outlook.co.nz>
2023-10-24 07:02:45 +1300

    before icon

--- extension.js
+++ extension.js
@@ -2,34 +2,56 @@ const vscode = require('vscode');
 
 function activate(context) {
     context.subscriptions.push(vscode.languages.registerHoverProvider('terraform', {
-        provideHover(document, position, token) {
+        provideHover(document, position) {
             const range = document.lineAt(position).range;
             const line = document.getText(range);
 
-            // Check if the line starts with 'resource' or 'data'
+            // Check if the line starts with 'resource', 'data', or 'provider'
             if (line.startsWith('resource') || line.startsWith('data')) {
-                const parts = line.split('"');
-                if (parts.length >= 5) {
-                    const provider = parts[1].split('_')[0];
-                    const type = parts[1].substring(parts[1].indexOf('_') + 1);
-                    const name = parts[3];
-
-                    // Construct the URL for the Terraform registry
-                    let url;
-                    if (line.startsWith('resource')) {
-                        url = `https://registry.terraform.io/providers/hashicorp/${provider}/latest/docs/resources/${type}`;
-                    } else { // data source
-                        url = `https://registry.terraform.io/providers/hashicorp/${provider}/latest/docs/data-sources/${type}`;
-                    }
-
-                    // Create the hover message
-                    const message = new vscode.MarkdownString(`Terraform registry lookup:  [${parts[1]}](${url})`);
-                    message.isTrusted = true;
-                    return new vscode.Hover(message);
-                }
+                return handleResourceOrData(line);
+            } else if (line.startsWith('provider')) {
+                return handleProvider(line);
             }
         }
     }));
+}
+
+// Handle 'resource' or 'data' lines
+function handleResourceOrData(line) {
+    const parts = line.split('"');
+    if (parts.length >= 5) {
+        const provider = parts[1].split('_')[0];
+        const type = parts[1].substring(parts[1].indexOf('_') + 1);
+
+        // Construct the URL for the Terraform registry
+        let url;
+        if (line.startsWith('resource')) {
+            url = `https://registry.terraform.io/providers/hashicorp/${provider}/latest/docs/resources/${type}`;
+        } else { // data source
+            url = `https://registry.terraform.io/providers/hashicorp/${provider}/latest/docs/data-sources/${type}`;
+        }
+
+        // Create the hover message
+        const message = new vscode.MarkdownString(`Terraform registry lookup:  [${parts[1]}](${url})`);
+        message.isTrusted = true;
+        return new vscode.Hover(message);
+    }
+}
+
+// Handle 'provider' lines
+function handleProvider(line) {
+    const parts = line.split('"');
+    if (parts.length >= 3) {
+        const provider = parts[1];
+
+        // Construct the URL for the Terraform registry
+        const url = `https://registry.terraform.io/providers/hashicorp/${provider}/latest`;
+
+        // Create the hover message
+        const message = new vscode.MarkdownString(`Terraform registry lookup:  ${provider}`);
+        message.isTrusted = true;
+        return new vscode.Hover(message);
+    }
 }
 
 // This method is called when your extension is deactivated
